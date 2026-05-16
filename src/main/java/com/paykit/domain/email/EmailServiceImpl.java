049c98ddf2f9942da03ac45ba7303d22e086623b
Shivam Gupta <tsg782@gmail.com>
2026-05-16 10:10:55 +0530

    configure Resend SMTP for email delivery

--- src/main/java/com/paykit/domain/email/EmailServiceImpl.java
+++ src/main/java/com/paykit/domain/email/EmailServiceImpl.java
@@ -1,7 +1,7 @@
 package com.paykit.domain.email;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -9,16 +9,23 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
+    private final String fromAddress;
+
+    public EmailServiceImpl(JavaMailSender mailSender,
+                            @Value("${spring.mail.from:onboarding@resend.dev}") String fromAddress) {
+        this.mailSender = mailSender;
+        this.fromAddress = fromAddress;
+    }
 
     @Override
     @Async("emailExecutor")
     public void sendInvoiceEmail(String to, String subject, String body) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromAddress);
             message.setTo(to);
             message.setSubject(subject);
             message.setText(body);
